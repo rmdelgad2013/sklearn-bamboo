fb587b224735ffb47f7b829098099878f93376e9
Ryan Delgado <ryandelgado8@gmail.com>
2018-04-07 16:51:32 -0400

    Added code to extract and insert the signature from the base transformer class into the wrapper class. This resolved the RuntimeError bug seen when using the transformer in a pipeline.

--- bamboo/preprocessing.py
+++ bamboo/preprocessing.py
@@ -1,20 +1,30 @@
+from inspect import signature
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import (StandardScaler, MinMaxScaler, Normalizer,
                                    MaxAbsScaler, RobustScaler, Binarizer, QuantileTransformer)
 
+def _extract_init_kwargs(obj):
+    '''Helper function to extract the signature from the sklearn Transformer class. This signature is then
+    Fed in to the DF extension Transformer class. This is done to handle this error:
+    	RuntimeError: scikit-learn estimators should always specify their parameters in the signature of their __init__
+    '''
+    sig = signature(obj.__init__)
+    return {sig.parameters[key].name: sig.parameters[key].default
+                for key in sig.parameters.keys()}
+
 NUMPY_NUMERIC_DTYPES = ('int8', 'int16', 'int32', 'int64', 'float16', 'float32', 'float64')
 
-# ToDo: Investigate and resolve this issue:
-#   RuntimeError: scikit-learn estimators should always specify their parameters in the signature of their __init__
-#   (no varargs). <class '__main__.MinMaxScalerDF'> with constructor (self, subset_columns=(), *args, **kwargs) doesn't  follow this convention.
+# ToDo: Write unit tests for common use cases - fit, then transform, fit_transform, pipeline, inverse_transform
 
 class NumericRescalerDF(BaseEstimator, TransformerMixin):
 
     _transformer_class = None
+    _transformer_init_kwargs = _extract_init_kwargs(_transformer_class)
 
-    def __init__(self, subset_columns=(), *args, **kwargs):
+    def __init__(self, subset_columns=(), **_transformer_init_kwargs):
         self.subset_columns = subset_columns
-        self._transformer = self._transformer_class(*args, **kwargs)
+        self._transformer = self._transformer_class(**_transformer_init_kwargs)
 
     def _is_all_numeric(self, X):
         return set(X[self.subset_columns].select_dtypes(include=NUMPY_NUMERIC_DTYPES).columns) == set(self.subset_columns)
@@ -39,7 +49,6 @@ class NumericRescalerDF(BaseEstimator, TransformerMixin):
         X_copy[self.subset_columns] = self._transformer.inverse_transform(X_copy[self.subset_columns].values)
         return X_copy
 
-# ToDo: Maybe look into a class factory function.
 
 class MinMaxScalerDF(NumericRescalerDF):
     _transformer_class = MinMaxScaler
@@ -62,3 +71,9 @@ class BinarizerDF(NumericRescalerDF):
 class QuantileTransformerDF(NumericRescalerDF):
     _transformer_class = QuantileTransformer
 
+
+class OneHotEncoderDF(BaseEstimator, TransformerMixin):
+    pass
+
+class PolynomialFeaturesDF(BaseEstimator, TransformerMixin):
+    pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sklearn-bamboo',
+    version='0.0.1',
+    packages=['bamboo'],
+    url='https://github.com/rmdelgad2013/sklearn-bamboo',
+    license='MIT',
+    author='Ryan Delgado',
+    author_email='ryandelgado8@gmail.com',
+    description='Custom sklearn Transformers on DataFrames instead of ndarrays'
+)
